98cd1f2e88ec50e9c19ab433516f82ebe541a414
Ryan Fox <ryan@foxrow.com>
2013-12-03 23:49:13 -0600

    added basic stacking

--- pypeline.py
+++ pypeline.py
@@ -1,21 +1,49 @@
+import math
+import sys
+
+from PIL import Image
+
 class Pypeline:
-    def __init(self, raws=None, flats=None, biases=None, darks=None):
-        '''Create a new pypeline object.'''
+    def __init__(self, raws=None, flats=None, biases=None, darks=None):
+        """Create a new pypeline object."""
         pass
 
     def calibrate(self, raw=None, flat=None, bias=None, dark=None):
-        '''Calibrate an image'''
+        """Calibrate an image"""
 
     def register(self, images=None):
-        '''Register (align) images'''
+        """Register (align) images"""
         pass
 
     def stack(self, images=None):
-        '''Stack images'''
-        pass
+        """Stack images"""
+        inputs = [Image.open(img) for img in images]
+        width, height = inputs[0].size
+        
+        out = Image.new("RGB", (width, height))
+        
+        for x in range(width):
+            for y in range(height):
+                pixels = [img.getpixel((x, y)) for img in inputs]
+                reds = [pixel[0] for pixel in pixels]
+                greens = [pixel[1] for pixel in pixels]
+                blues = [pixel[2] for pixel in pixels]
+                
+                reds.sort()
+                greens.sort()
+                blues.sort()
 
+                medianred = reds[math.floor(len(reds) / 2)]
+                mediangreen = greens[math.floor(len(greens) / 2)]
+                medianblue = blues[math.floor(len(blues) / 2)]
+                
+                out.putpixel((x,y), (medianred, mediangreen, medianblue))
+                
+        return out
 
 
 if __name__ == '__main__':
-    p = Pypeline()
-
+    pype = Pypeline()
+    newimg = pype.stack(sys.argv[1:-1])
+    newimg.save(sys.argv[-1], "JPEG")
+    
